a16ffe93524fd67b0ff39353aa1a9fa7fed9081d
Liam Evans <51797340+Levans18@users.noreply.github.com>
2025-03-29 12:04:38 -0400

    Fixed Warnings In RegisterDto

--- api/DTOs/RegisterDto.cs
+++ api/DTOs/RegisterDto.cs
@@ -5,14 +5,14 @@ namespace FitStack.API.DTOs
     public class RegisterDto
     {
         [Required]
-        public string Username {get; set;} = null;
+        public string Username {get; set;} = null!;
 
         [Required]
         [EmailAddress]
-        public string Email {get; set;} = null;
+        public string Email {get; set;} = null!;
 
         [Required]
-        public string Password {get; set; } = null;
+        public string Password {get; set; } = null!;
     }
 
 }
